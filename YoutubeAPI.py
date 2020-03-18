91ab3ac7d4b5eb79616af6267ed0c65aa18862d4
Psycho <laurentchervet@bluewin.ch>
2020-03-18 19:14:50 +0100

    First part

--- YoutubeAPI.py
+++ YoutubeAPI.py
@@ -1,3 +1,8 @@
+from typing import Optional
+
+from youtube_api import YouTubeDataAPI
+
+from core.ProjectAliceExceptions import SkillStartingFailed
 from core.base.model.AliceSkill import AliceSkill
 from core.dialog.model.DialogSession import DialogSession
 from core.util.Decorators import IntentHandler
@@ -9,6 +14,30 @@ class YoutubeAPI(AliceSkill):
 	Description: Access and manage your youtube account
 	"""
 
-	@IntentHandler('MyIntentName')
-	def dummyIntent(self, session: DialogSession, **_kwargs):
-		pass
+
+	def __init__(self):
+		super().__init__()
+		try:
+			self._youtube: Optional[YouTubeDataAPI] = None
+		except ValueError:
+			raise SkillStartingFailed('Youtube api key not valid')
+
+
+	def onStart(self):
+		super().onStart()
+		self._youtube = YouTubeDataAPI(self.getConfig('youtubeApiKey'))
+		if not self._youtube.verify_key():
+			raise SkillStartingFailed('Youtube api key not valid')
+
+
+	@IntentHandler('GetYoutubeChannelStats')
+	def getChannelStats(self, session: DialogSession, **_kwargs):
+		metadata = self._youtube.get_channel_metadata('')
+
+		if not metadata:
+			self.endDialog(sessionId=session.sessionId, text=self.randomTalk(text='error'))
+			return
+
+		print(metadata)
+
+		self.endDialog(sessionId=session.sessionId, text=self.randomTalk(text='myText', replace=[sub]))
